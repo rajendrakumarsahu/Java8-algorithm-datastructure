1afab339954a8d9c7743595ad0010f5cac7b5988
rajendrakumarsahu <rajendrakumarsahu1986@gmail.com>
2024-06-18 10:54:25 +0530

    committed

--- src/com/algorithm/ArmstrongNumber.java
+++ src/com/algorithm/ArmstrongNumber.java
@@ -3,32 +3,41 @@ package com.algorithm;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		int inputArmstrongNumber = 153; // Input number to check armstrong
-		int tempNumber, digit, digitCubeSum = 0;
-
-		tempNumber = inputArmstrongNumber;
-		while (tempNumber != 0) {
-
-			/*
-			 * On each iteration, remainder is powered by thetempNumber of digits n
-			 */
-			System.out.println("Current Number is " + tempNumber);
-			digit = tempNumber % 10;
-			System.out.println("Current Digit is " + digit);
-			// sum of cubes of each digits is equal to thetempNumber itself
-			digitCubeSum = digitCubeSum + digit * digit * digit;
-			System.out.println("Current digitCubeSum is " + digitCubeSum);
-			tempNumber /= 10;
-			System.out.println("Current Number is after::" + tempNumber);
-
-		}
-
-		// check giventempNumber and digitCubeSum is equal to or not
-		if (digitCubeSum == inputArmstrongNumber)
+		int inputArmstrongNumber = 1634; // Input number to check armstrong
+		if (isArmstrong(inputArmstrongNumber))
 			System.out.println(inputArmstrongNumber + " is an Armstrong Number");
 		else
 			System.out.println(inputArmstrongNumber + " is not an Armstrong Number");
 
 	}
 
+	static boolean isArmstrong(int x) {
+		// Calling order function
+		int n = order(x);
+		int temp = x, sum = 0;
+		while (temp != 0) {
+			int r = temp % 10;
+			sum = (int) (sum + Math.pow(r, n));
+			temp = temp / 10;
+		}
+
+		// If satisfies Armstrong condition
+		return (sum == x);
+	}
+
+	static int order(int x) {
+		int n = 0;
+		while (x != 0) {
+			n++;
+			x = x / 10;
+		}
+		return n;
+	}
+
+	private static boolean isArmstrongNumberByJava8(int n) {
+		int len = String.valueOf(n).length();
+		int sum = String.valueOf(n).chars().map(ch -> Character.digit(ch, 10)).map(digit -> (int) Math.pow(digit, len))
+				.sum();
+		return sum == n;
+	}
 }
